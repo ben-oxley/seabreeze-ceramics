88965340cdb324149cfd19a47cc80627c0ef6962
Ben Oxley <ben.oxley@gmail.com>
2015-02-17 23:08:01 +0000

    Added dynamic image loading

---
+++ previewer.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("#red").click(function(){
+    	 $("#previewWindow").html($("<img>").attr("src", "./images/print_basic.jpg").attr("class","img-circle").attr("id","imgBlurred")); 
+
+    });
+});
