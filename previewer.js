923ddd83dbe38dba69495729cbb0198e5ef280c3
Ben Oxley <ben.oxley@gmail.com>
2015-02-21 19:36:39 +0000

    Fixed layout and added new images

--- previewer.js
+++ previewer.js
@@ -1,12 +1,24 @@
 $(document).ready(function(){
 	//$("#previewWindow").html($("<img>").attr("src", "./images/tile_background.png")); 
-    $("#blue").click(function(){
-    	 $("#pawForeground").html($("<img>").attr("src", "./images/paw_blue.png").attr("class","overlay")); 
+    $("#pawBrown").click(function(){
+    	 $("#pawForeground").html($("<img>").attr("src", "./images/paw_brown.png").attr("class","overlay")); 
     });
-    $("#green").click(function(){
-    	 $("#pawForeground").html($("<img>").attr("src", "./images/paw_green.png").attr("class","overlay")); 
+    $("#pawCream").click(function(){
+    	 $("#pawForeground").html($("<img>").attr("src", "./images/paw_cream.png").attr("class","overlay")); 
     });
-    $("#red").click(function(){
-    	 $("#pawForeground").html($("<img>").attr("src", "./images/paw_red.png").attr("class","overlay")); 
+    $("#pawGrey").click(function(){
+    	 $("#pawForeground").html($("<img>").attr("src", "./images/paw_grey.png").attr("class","overlay")); 
+    });
+    $("#textLilac").click(function(){
+         $("#textForeground").html($("<img>").attr("src", "./images/text_lilac.png").attr("class","overlay")); 
+    });
+    $("#textBlue").click(function(){
+         $("#textForeground").html($("<img>").attr("src", "./images/text_blue.png").attr("class","overlay")); 
+    });
+    $("#textGrey").click(function(){
+         $("#textForeground").html($("<img>").attr("src", "./images/text_grey.png").attr("class","overlay")); 
+    });
+    $("#textBrown").click(function(){
+         $("#textForeground").html($("<img>").attr("src", "./images/text_brown.png").attr("class","overlay")); 
     });
 });
