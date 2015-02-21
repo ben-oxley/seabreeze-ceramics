4f5727d8be667d2107618e5b3a4f41ae6a798995
Ben Oxley <ben.oxley@gmail.com>
2015-02-21 18:17:41 +0000

    Updated preview system

--- previewer.js
+++ previewer.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
+	//$("#previewWindow").html($("<img>").attr("src", "./images/tile_background.png")); 
+    $("#blue").click(function(){
+    	 $("#pawForeground").html($("<img>").attr("src", "./images/paw_blue.png").attr("class","overlay")); 
+    });
+    $("#green").click(function(){
+    	 $("#pawForeground").html($("<img>").attr("src", "./images/paw_green.png").attr("class","overlay")); 
+    });
     $("#red").click(function(){
-    	 $("#previewWindow").html($("<img>").attr("src", "./images/print_basic.jpg").attr("class","img-circle").attr("id","imgBlurred")); 
-
+    	 $("#pawForeground").html($("<img>").attr("src", "./images/paw_red.png").attr("class","overlay")); 
     });
 });
