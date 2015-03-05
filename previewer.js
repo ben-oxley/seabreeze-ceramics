20431c4f91db81bf3a3e63935c98f62a4eae2f83
Ben Oxley <ben.oxley@gmail.com>
2015-03-05 21:38:35 +0000

    Updated icons and added paypal selector

--- previewer.js
+++ previewer.js
@@ -1,4 +1,7 @@
+var frame = false;
+
 $(document).ready(function(){
+    loadPrice(true);
 	//$("#previewWindow").html($("<img>").attr("src", "./images/tile_background.png")); 
     $("#pawBrown").click(function(){
     	 $("#pawForeground").html($("<img>").attr("src", "./images/paw_brown.png").attr("class","overlay")); 
@@ -23,11 +26,55 @@ $(document).ready(function(){
     });
     $("#framePine").click(function(){
          $("#frameForeground").html($("<img>").attr("src", "./images/frame_pine_500.png").attr("class","overlay")); 
+        loadPrice(true);
     });
     $("#frameBlack").click(function(){
          $("#frameForeground").html($("<img>").attr("src", "./images/frame_black_500.png").attr("class","overlay")); 
+        loadPrice(true);
+    });
+    $("#frameNone").click(function(){
+         $("#frameForeground").html($("")); 
+         loadPrice(false);
     });
     $("#frameWhite").click(function(){
          $("#frameForeground").html($("<img>").attr("src", "./images/frame_white_500.png").attr("class","overlay")); 
+         loadPrice(true);
     });
+
 });
+
+function loadPrice(frame){
+    if(frame === false){
+            $("#paypalLink").html($(" <div class=\"col-lg-2\"><p>Small Unframed - £34.50</p>\
+            <form action=\"https://www.paypal.com/cgi-bin/webscr\" method=\"post\" target=\"_top\">\
+            <input type=\"hidden\" name=\"cmd\" value=\"_s-xclick\">\
+            <input type=\"hidden\" name=\"hosted_button_id\" value=\"6PUG3BBUSSMJC\">\
+            <input type=\"image\" src=\"https://www.paypalobjects.com/en_US/GB/i/btn/btn_buynowCC_LG.gif\" border=\"0\" name=\"submit\" alt=\"PayPal – The safer, easier way to pay online.\">\
+            <img alt=\"\" border=\"0\" src=\"https://www.paypalobjects.com/en_GB/i/scr/pixel.gif\" width=\"1\" height=\"1\">\
+            </form></div>\
+            <div class=\"col-lg-2\">\
+            <p>Large Unframed - £69.50</p>\
+            <form action=\"https://www.paypal.com/cgi-bin/webscr\" method=\"post\" target=\"_top\">\
+            <input type=\"hidden\" name=\"cmd\" value=\"_s-xclick\">\
+            <input type=\"hidden\" name=\"hosted_button_id\" value=\"6PUG3BBUSSMJC\">\
+            <input type=\"image\" src=\"https://www.paypalobjects.com/en_US/GB/i/btn/btn_buynowCC_LG.gif\" border=\"0\" name=\"submit\" alt=\"PayPal – The safer, easier way to pay online.\">\
+            <img alt=\"\" border=\"0\" src=\"https://www.paypalobjects.com/en_GB/i/scr/pixel.gif\" width=\"1\" height=\"1\">\
+            </form></div>"));
+    } else if (frame === true) {
+            $("#paypalLink").html($("<div class=\"col-lg-2\"><p>Small Framed - £54.50</p>\
+            <form action=\"https://www.paypal.com/cgi-bin/webscr\" method=\"post\" target=\"_top\">\
+            <input type=\"hidden\" name=\"cmd\" value=\"_s-xclick\">\
+            <input type=\"hidden\" name=\"hosted_button_id\" value=\"6PUG3BBUSSMJC\">\
+            <input type=\"image\" src=\"https://www.paypalobjects.com/en_US/GB/i/btn/btn_buynowCC_LG.gif\" border=\"0\" name=\"submit\" alt=\"PayPal – The safer, easier way to pay online.\">\
+            <img alt=\"\" border=\"0\" src=\"https://www.paypalobjects.com/en_GB/i/scr/pixel.gif\" width=\"1\" height=\"1\">\
+            </form></div>\
+            <div class=\"col-lg-2\">\
+            <p>Large Framed - £89.50</p>\
+            <form action=\"https://www.paypal.com/cgi-bin/webscr\" method=\"post\" target=\"_top\">\
+            <input type=\"hidden\" name=\"cmd\" value=\"_s-xclick\">\
+            <input type=\"hidden\" name=\"hosted_button_id\" value=\"6PUG3BBUSSMJC\">\
+            <input type=\"image\" src=\"https://www.paypalobjects.com/en_US/GB/i/btn/btn_buynowCC_LG.gif\" border=\"0\" name=\"submit\" alt=\"PayPal – The safer, easier way to pay online.\">\
+            <img alt=\"\" border=\"0\" src=\"https://www.paypalobjects.com/en_GB/i/scr/pixel.gif\" width=\"1\" height=\"1\">\
+            </form></div>"));
+    }
+}
--- readme.md
+++ readme.md
@@ -1,3 +1,11 @@
 #Seabreeze Ceramics
 
 This is the website repository for [seabreezeceramic.co.uk]. A business making handmade animal pawprint ceramic tiles and other unique gifts.
+
+Payment Processing
+1. Select product on website by size and frame price is produced per product.
+2. Forwarded to payment processing size.
+3. Payment details and order are forwarded to website, this information is verified and added to the database.
+4. Customer is informed via email and given a tracking number for their order.
+5. Order can be tracked from the frontend by the customer using the unique number without login. 
+6. From the backend and with authentication, the order status can be changed and the details viewed.
