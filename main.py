b279b9682c2e406ebc8349c6af36294d00506090
Ben Oxley <ben.oxley@gmail.com>
2015-03-15 22:24:12 +0000

    Progress on demonstrating AJAX page loading

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, jsonify, render_template, request
 import paypalrestsdk
 import json
 # set the project root directory as the static folder, you can set others.
@@ -11,7 +11,7 @@ def send_js():
 if __name__ == "__main__":
     app.run()
 
-@app.route('/paypal')
+@app.route('/paypalexample')
 def pull_payments():
 	paypalrestsdk.configure({
 	  "mode": "sandbox", # sandbox or live
@@ -32,3 +32,25 @@ def pull_payments():
 		# newStringOfResults += str(paymentEntry.payer.payer_info.shipping_address.postal_code)
 		newStringOfResults += "<br>"	
 	return newStringOfResults
+
+@app.route('/paypalpayments')
+def getAllPayments():
+	paypalrestsdk.configure({
+	  "mode": "sandbox", # sandbox or live
+	  "client_id": "EBWKjlELKMYqRNQ6sYvFo64FtaRLRR5BdHEESmha49TM",
+	  "client_secret": "EO422dn3gQLgDbuwqTjzrFgFtaRLRR5BdHEESmha49TM" })
+	payment = paypalrestsdk.Payment.find("PAY-57363176S1057143SKE2HO3A")
+
+	# Get List of Payments
+	payment_history = paypalrestsdk.Payment.all({"count": 10})
+	return str(payment_history.payments)
+
+@app.route('/paypal/')
+def showPayments():
+	return render_template('paypal.html')
+
+@app.route('/_add_numbers')
+def add_numbers():
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    return jsonify(result=a + b)
